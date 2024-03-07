dc848f11089ef969395c61775b678f5ffd7f8805
Phongvo123 <19522015@gm.uit.edu.vn>
2024-03-07 17:09:29 +0700

    SignUp UI

--- client/src/pages/LogInPage/LogInPage.js
+++ client/src/pages/LogInPage/LogInPage.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ModalComponent from '../../components/ModalComponent/ModalComponent'
 
 const LogInPage = () => {
+    const [isOpenModal, setIsOpenModal] = useState(false)
+
+    const handleOpenModal = () => {
+        setIsOpenModal(true)
+    }
+
+    const handleCancelModal = () => {
+        setIsOpenModal(false)
+    }
+
   return (
     <div style={{height : "720px", background: "#f0f2f5"}} className='d-flex flex-column align-items-center justify-content-center'>
         <h1 className='mb-3' style={{color: "#0866ff"}}>Social Media</h1>
@@ -17,11 +28,33 @@ const LogInPage = () => {
                 </div>
                 <div className='form-group mb-3' style={{borderBottom: "1px solid #dadde1"}}></div>
                 <div className='form-group mb-3 text-center'>
-                    <button type='button' className='btn w-60 btn-lg' style={{background: "#42b72a", color: "#fff"}}>Create new account</button>
+                    <button type='button' className='btn w-60 btn-lg' style={{background: "#42b72a", color: "#fff"}}
+                    onClick={handleOpenModal}
+                    >
+                        Create new account
+                    </button>
                 </div>
             </form>
+            <ModalComponent title='Sign Up' isOpen={isOpenModal} footer={null} onCancel={handleCancelModal}>
+                <div className='mb-2' style={{color: "#606770"}}>It's quick and easy</div>
+                <div className='mb-3' style={{borderBottom: "1px solid #dadde1"}}></div>
+                <form>
+                <div className='form-group mb-2 d-flex justify-content-between gap-2'>
+                    <input type='text' className='w-50 form-control' placeholder='Firstname' style={{background: "#f5f6f7"}}/>
+                    <input type='text' className='w-50 form-control' placeholder='Lastname' style={{background: "#f5f6f7"}}/>
+                </div>
+                <div className='form-group mb-2'>
+                    <input type='text' className='w-100 form-control' placeholder='Username' style={{background: "#f5f6f7"}}/>
+                </div>
+                <div className='form-group mb-3'>
+                <input type='password' className='w-100 form-control' placeholder='Password' style={{background: "#f5f6f7"}}/>
+                </div>
+                <div className='form-group mb-3 text-center'>
+                <button type="button" className="btn w-70 btn-lg" style={{background: "#42b72a", color: "#fff"}} >Sign Up</button>
+                </div>
+            </form>
+            </ModalComponent>
         </div>
-        
     </div>
   )
 }
